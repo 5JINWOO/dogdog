caf356a2bbc9c2b23a3f3bd04c3965eae4f1eb06
5JINWOO <etetqlql@gmail.com>
2022-02-20 23:43:36 +0900

    gukbi project

---
+++ myscript.js
@@ -0,0 +1,29 @@
+
+//  헤더 스크립트
+const toggleBtn = document.querySelector('.navbar_toogleBtn');
+        const menu = document.querySelector('.navbar_menu');
+        const icons = document.querySelector('.navbar_icons');
+
+        toggleBtn.addEventListener('click', () => {
+            menu.classList.toggle('active');
+            icons.classList.toggle('active');
+        });
+
+// 로그인 페이지 section
+
+
+    
+    function loginCheck(){
+        const id = document.querySelector('.user_id');
+        const pw = document.querySelector('.user_pw');
+        const link = document.write("<a href = 'https://www.naver.com/'" );
+        if(id.value==""){
+            alert("아이디를 입력하세요");
+        }
+        else if(pw.value==""){
+            alert("비밀번호를 입력하세요");
+        }
+        else {
+            alert("로그인 성공!");            
+        }
+    }
